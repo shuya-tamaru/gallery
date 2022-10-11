2dae47b67d6bcfbe75a3d2a1ca344bcf55ec300d
shuya-tamaru <tamaru1013@gmail.com>
2022-10-11 21:55:10 +0900

    cameraMove

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,7 +8,13 @@ const Home: NextPage = () => {
     const canvas = document.querySelector("#canvas") as HTMLCanvasElement;
     const experience = new Experience(canvas);
   }, []);
-  return <canvas id="canvas" className={styles.canvas}></canvas>;
+  return (
+    <>
+      <canvas id="canvas" className={styles.canvas} style={{ position: "relative" }}></canvas>
+      <button className="button" id="plus" style={{ width: "100px", height: "50px", display: "flex", zIndex: "10", backgroundColor: "gray", position: "absolute", top: "0%", left: "0%", cursor: "pointer" }}>ボタンプラス</button>
+      <button className="button" id="minus" style={{ width: "100px", height: "50px", display: "flex", zIndex: "10", backgroundColor: "gray", position: "absolute", top: "10%", left: "0%", cursor: "pointer" }}>ボタンマイナス</button>
+    </>
+  );
 };
 
 export default Home;
--- threeScripts/Experience/Camera.ts
+++ threeScripts/Experience/Camera.ts
@@ -1,5 +1,6 @@
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import * as TWEEN from '@tweenjs/tween.js'
 
 import Experience from "./Experience";
 import Sizes from "./Utils/Sizes";
@@ -11,25 +12,70 @@ export default class Camera {
   canvas: HTMLCanvasElement | undefined;
   instance: THREE.PerspectiveCamera | undefined;
   controls: OrbitControls | undefined;
+  cameraPos: { x: number, y: number, z: number }[];
+  cameraPosIndex: number;
+  tween: any;
 
   constructor() {
     this.experience = new Experience();
     this.sizes = this.experience.sizes;
     this.scene = this.experience.scene;
     this.canvas = this.experience.canvas;
+    this.cameraPosIndex = 0;
+
+    this.cameraPos = [
+      { x: 0, y: 4, z: 8 },//initial 0
+      { x: -5, y: 0.5, z: 3 },//gallery 1
+      { x: -3, y: 2.3, z: 3 },//movinSphere 2
+      { x: -0.5, y: 1.4, z: 3 },//hexDome 3
+      { x: 1.6, y: 0.5, z: 3 },//twisArc 4
+      { x: 4.2, y: 1.4, z: 3 },//curtain 5
+      { x: 6.0, y: 3.5, z: 3.2 },//lamp 6
+      { x: 3.2, y: 4.0, z: 3.0 },//pipe 7
+      { x: 1.0, y: 3.2, z: 3.0 },//feather 8
+      { x: -1.5, y: 4.0, z: 3.0 },//ribbon 9
+      { x: -4.1, y: 4.2, z: 3.2 },//waffle 10
+      { x: -3.1, y: 7.0, z: 3.2 },//origami 11
+      { x: -0.8, y: 6.0, z: 3.2 },//town 12
+      { x: 1.8, y: 5.8, z: 3.2 },//deformSphere 13
+      { x: 4.4, y: 6.4, z: 3.2 },//fin 14
+    ]
 
     this.setInstance();
-    this.setOrbitComtrols();
+    // this.setOrbitComtrols();
+
+    const button = document.getElementsByClassName('button');
+    for (let i = 0; i < button.length; i++) {
+      button[i].addEventListener('click', () => {
+        if (button[i].id === "plus") {
+          this.cameraPosIndex === 14 ? this.cameraPosIndex = 0 : this.cameraPosIndex++;
+        } else {
+          this.cameraPosIndex === 0 ? this.cameraPosIndex = 14 : this.cameraPosIndex--;
+        }
+
+        this.tween = new TWEEN.Tween(this.instance!.position).to({
+          x: this.cameraPos[this.cameraPosIndex].x,
+          y: this.cameraPos[this.cameraPosIndex].y,
+          z: this.cameraPos[this.cameraPosIndex].z,
+        }, 1500)
+          .onUpdate((coords) => {
+            this.instance!.position.set(coords.x, coords.y, coords.z)
+          })
+          .easing(TWEEN.Easing.Exponential.Out)
+          .start();
+
+      })
+    }
   }
 
   setInstance() {
     this.instance = new THREE.PerspectiveCamera(
-      35,
+      75,
       this.sizes!.width / this.sizes!.height,
       0.1,
       100
     );
-    this.instance.position.set(10, 10, 25);
+    this.instance.position.set(this.cameraPos[this.cameraPosIndex].x, this.cameraPos[this.cameraPosIndex].y, this.cameraPos[this.cameraPosIndex].z);
     this.scene?.add(this.instance);
   }
   setOrbitComtrols() {
@@ -37,6 +83,10 @@ export default class Camera {
       this.controls = new OrbitControls(this.instance, this.canvas);
       this.controls.enableDamping = true;
     }
+  }
+
+  cameraTween() {
+
   }
 
   resize() {
@@ -48,5 +98,6 @@ export default class Camera {
 
   update() {
     this.controls?.update();
+    this.tween?.update();
   }
 }
